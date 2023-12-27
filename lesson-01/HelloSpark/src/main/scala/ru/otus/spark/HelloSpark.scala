1d5c1d8b17cb5447e198175029d84598fac803db
Vadim Zaigrin <vzaigrin@yandex.ru>
2023-12-27 10:30:05 +0300

    Update example fro Lesson 1

--- lesson-01/HelloSpark/src/main/scala/ru/otus/spark/HelloSpark.scala
+++ lesson-01/HelloSpark/src/main/scala/ru/otus/spark/HelloSpark.scala
@@ -1,7 +1,10 @@
+package ru.otus.spark
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import java.time._
 import java.sql.Timestamp
+import java.time._
+import scala.io.StdIn
 
 object HelloSpark {
   def main(args: Array[String]): Unit = {
@@ -16,9 +19,9 @@ object HelloSpark {
     println(s"spark.version == ${spark.version}")
 
     val df = List(
-      (1L, 2.0, "string1", LocalDate.of(2000, 1, 1), Timestamp.valueOf("2000-01-01 12:00:00")),
-      (2L, 3.0, "string2", LocalDate.of(2000, 2, 1), Timestamp.valueOf("2000-02-01 12:00:00")),
-      (3L, 4.0, "string3", LocalDate.of(2000, 3, 1), Timestamp.valueOf("2000-03-01 12:00:00"))
+      (1L, 2.0, "string1", LocalDate.of(2000, 1, 1), Timestamp.valueOf("2023-01-01 12:00:00")),
+      (2L, 3.0, "string2", LocalDate.of(2000, 2, 1), Timestamp.valueOf("2023-02-01 12:00:00")),
+      (3L, 4.0, "string3", LocalDate.of(2000, 3, 1), Timestamp.valueOf("2023-03-01 12:00:00"))
     ).toDF("a", "b", "c", "d", "e")
 
     df.printSchema()
@@ -27,6 +30,10 @@ object HelloSpark {
     df.withColumn("upper_c", upper($"c")).show()
     df.select(col("c")).show()
     df.filter($"a" === 1L).show()
+
+    // Ожидаем ввода для подтверждения окончания
+    println("Всё\nДля завершения работы нажмите Ввод")
+    StdIn.readLine()
 
     spark.stop()
   }
