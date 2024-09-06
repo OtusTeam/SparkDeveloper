d06b0afc9248b795dcf335544e4a25277c8def62
Вадим Заигрин <vzaigrin@yandex.ru>
2024-09-06 21:36:40 +0300

    Update Dataset and SQL examples

--- lesson-12/Dataset/SparkDatasetAPI/src/main/scala/ru/otus/spark/Cust.scala
+++ lesson-12/Dataset/SparkDatasetAPI/src/main/scala/ru/otus/spark/Cust.scala
@@ -2,6 +2,7 @@ package ru.otus.spark
 
 import java.sql.Date
 import java.text.SimpleDateFormat
+import java.util
 
 case class Cust(
     CustomerID: Long,
@@ -13,8 +14,8 @@ case class Cust(
 object Cust {
   private val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
   def apply(c: Customers): Cust = {
-    val utilDate = format.parse(c.Birthdate)
-    val sqlDate  = new Date(utilDate.getTime)
+    val utilDate: util.Date = format.parse(c.Birthdate)
+    val sqlDate: Date       = new Date(utilDate.getTime)
     Cust(
       c.CustomerID.toLong,
       c.Name,
